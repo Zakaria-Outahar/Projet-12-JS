70efd5d0d2f9e3e08f587bfb817f580a59c260e3
Zakaria Outahar <outaharz@gmail.com>
2021-09-11 11:01:00 +0200

    Fin du projet : supprimer une image proposée, en sélectionner 3 au total

--- app.js
+++ app.js
@@ -3,31 +3,69 @@ const premiereCase = document.getElementById('premiere-case');
 const boxs = document.querySelectorAll('.case');
 const destroy = document.querySelector('.destroy');
 const allCases = [];
+let photoChoisies = 0;
 
 for (let i = 0; i < boxs.length; i++) {
     allCases.push(boxs[i]);
     
 }
+allCases.push(destroy);
 
-let indexPhoto = 1;
+let indexPhoto = Math.floor(Math.random()*300);
 
-base.style.backgroundImage = `url(https://loremflickr.com/320/240?random=${indexPhoto})`;
+base.style.backgroundImage = `url(https://picsum.photos/id/${indexPhoto}/200/300)`;
+
+function nvBase(){
+    const newBase = document.createElement('div');
+    newBase.setAttribute('class','base');
+    newBase.setAttribute('draggable','true');
+    indexPhoto++;
+    newBase.style.backgroundImage = `url(https://picsum.photos/id/${indexPhoto}/200/300)`;
+    premiereCase.appendChild(newBase);
+    base = newBase;
+}
 
 for(const vide of allCases){
     vide.addEventListener('dragover', dragOver);
     vide.addEventListener('dragenter', dragEnter);
     vide.addEventListener('drop', dragDrop);
+  
 }
 
 function dragDrop(){
-    this.appendChild(base);    
+    if(this.id === "premiere-case"){
+        return;
+    }
+
+    // Destroy
+    if(this.id === "destroy"){
+        base.remove();
+        nvBase();
+        return;
+    }
+
+    // Verouillage
+    this.removeEventListener('drop', dragDrop);
+    this.removeEventListener('dragenter', dragEnter);
+    this.removeEventListener('dragover', dragOver);
+
+    this.appendChild(base); 
+    photoChoisies++;
+    this.childNodes[0].setAttribute('draggable', false);
+    if(photoChoisies === 3){
+        setTimeout(() => {
+            alert("Sélection terminée !");
+        }, 200);
+    } else{
+        nvBase();  
+    }
 }
 
 function dragOver(e){
     e.preventDefault();
 }
 
-function dragEnter(){
+function dragEnter(e){
     e.preventDefault();
 }
 
